d2f03c24837c90330c4614f1844fe975123900ab
Arthur Candido <oarthurcandido@gmail.com>
2022-12-03 14:03:10 -0300

    :construction: eslint setup

--- projects/web-public/pages/index.tsx
+++ projects/web-public/pages/index.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
-import { sum } from "@alura/utils/math/sum"
-import { Text } from "@alura/design-system/components/Text"
+import React from "react";
+import { sum } from "@alura/utils/math/sum";
+import { Text } from "@alura/design-system/components/Text";
 
 export default function HomeScreen() {
   return (
     <div>
-      <Text tag={'h1'}>Home</Text>
-      <Text tag={'p'}>Importando módulo local: @alura/utils/math/sum sum(2,2): {sum(2, 2)}</Text>
+      <Text tag={"h1"}>Home</Text>
+      <a href="https://google.com">Aqui</a>
+      <Text tag={"p"}>
+        Importando módulo local: @alura/utils/math/sum sum(2,2): {sum(2, 2)}
+      </Text>
     </div>
-  )
+  );
 }
