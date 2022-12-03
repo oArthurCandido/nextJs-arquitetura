84c0e2c2b48dc4986493810f7f27f7726a9aab21
Arthur Candido <oarthurcandido@gmail.com>
2022-12-03 11:45:18 -0300

    :construction: yarn workspaces reutilização de setups next-transpile-modules

--- projects/web-public/pages/index.tsx
+++ projects/web-public/pages/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { sum } from "@alura/utils/math/sum"
+import { Text } from "@alura/design-system/components/Text"
 
 export default function HomeScreen() {
   return (
     <div>
-      <h1>Home</h1>
-      <p>Some content</p>
+      <Text tag={'h1'}>Home</Text>
+      <Text tag={'p'}>Importando módulo local: @alura/utils/math/sum sum(2,2): {sum(2, 2)}</Text>
     </div>
   )
 }
